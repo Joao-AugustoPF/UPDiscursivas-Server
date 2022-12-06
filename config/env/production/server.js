e8b68ae9cfab86b0e20d7f986bd58a34aa618580
João Augusto <joaoaugustopfpf@gmail.com>
2022-12-06 12:44:45 -0300

    tenth commit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   url: env(process.env.PRODUCTION_HOST),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    key: env.array('APP_KEYS', ['Y31WH3XkmBZ7I+bovzf/Eg==','dJJrPcPKttTWqejnpJ6ZOg==','xmtSos6JhJSfk/prC5MwuA==','3DzGG5wmjI2cDjac/0WcUw==']),
   }
 });
