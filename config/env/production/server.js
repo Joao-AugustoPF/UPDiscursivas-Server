88031c7b79f2036210f30dfc9f19bd9ff7d84c8c
João Augusto <joaoaugustopfpf@gmail.com>
2022-12-06 13:22:01 -0300

    fifteenth commit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,15 @@
 //a
 module.exports = ({ env }) => ({
+  host: env('HOST', process.env.PRODUCTION_HOST),
+  port: env(process.env.PORT),
   url: env(process.env.PRODUCTION_HOST),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS', ['Y31WH3XkmBZ7I+bovzf/Eg==','dJJrPcPKttTWqejnpJ6ZOg==','xmtSos6JhJSfk/prC5MwuA==','3DzGG5wmjI2cDjac/0WcUw==']),
-  }
+  },
+  admin: {
+    auth: {
+      secret: env(process.env.JWT_SECRET),
+    },
+  },
 });
