891fdf347add483be9019faefba73cee5c85e26c
João Augusto <joaoaugustopfpf@gmail.com>
2022-12-06 13:09:48 -0300

    fouteenth commit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
   url: env(process.env.PRODUCTION_HOST),
   proxy: true,
   app: {
-    key: env.array('APP_KEYS', ['Y31WH3XkmBZ7I+bovzf/Eg==','dJJrPcPKttTWqejnpJ6ZOg==','xmtSos6JhJSfk/prC5MwuA==','3DzGG5wmjI2cDjac/0WcUw==']),
+    keys: env.array('APP_KEYS', ['Y31WH3XkmBZ7I+bovzf/Eg==','dJJrPcPKttTWqejnpJ6ZOg==','xmtSos6JhJSfk/prC5MwuA==','3DzGG5wmjI2cDjac/0WcUw==']),
   }
 });
--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_HOST : process.env.HOST),
   port: env.int('PORT', process.env.PORT || 1337),
   app: {
-    key: env.array('APP_KEYS', ['Y31WH3XkmBZ7I+bovzf/Eg==','dJJrPcPKttTWqejnpJ6ZOg==','xmtSos6JhJSfk/prC5MwuA==','3DzGG5wmjI2cDjac/0WcUw==']),
+    keys: env.array('APP_KEYS', ['Y31WH3XkmBZ7I+bovzf/Eg==','dJJrPcPKttTWqejnpJ6ZOg==','xmtSos6JhJSfk/prC5MwuA==','3DzGG5wmjI2cDjac/0WcUw==']),
   }
 });
