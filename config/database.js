b6bf72233484a1774a87a8e1dedaa899a6ef9c08
João Augusto <joaoaugustopfpf@gmail.com>
2022-12-06 11:53:26 -0300

    fifth commit

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', process.env.NODE_ENV === 'production' ? process.env.DATABASE_PROUDCTION_HOST : process.env.DATABASE_LOCAL_HOST),
-      port: env.int('DATABASE_PORT', process.env.NODE_ENV === 'production' ? process.env.DATABASE_PRODUCTION_PORT : process.env.DATABASE_LOCAL_PORT),
-      database: env('DATABASE_NAME', process.env.NODE_ENV === 'production' ? process.env.DATABASE_PRODUCTION_DATABASE : process.env.DATABASE_LOCAL_DATABASE),
-      user: env('DATABASE_USERNAME', process.env.NODE_ENV === 'production' ? process.env.DATABASE_PRODUCTION_USER : process.env.DATABASE_LOCAL_USER),
-      password: env('DATABASE_PASSWORD', process.env.NODE_ENV === 'production' ? process.env.DATABASE_PRODUCTION_PASSWORD : process.env.DATABASE_LOCAL_PASSWORD),
+      host: env('DATABASE_HOST', process.env.DATABASE_PROUDCTION_HOST),
+      port: env.int('DATABASE_PORT', process.env.DATABASE_PRODUCTION_PORT),
+      database: env('DATABASE_NAME', process.env.DATABASE_PRODUCTION_DATABASE),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_PRODUCTION_USER),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PRODUCTION_PASSWORD),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       }
